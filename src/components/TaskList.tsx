4443bdd190b78a6111233c7949d33a5fd5190a8b
sandropimenta-hotmart <sandro.pimenta@hotmart.com>
2022-02-28 09:28:04 -0300

    feat: create task, complete task and delete task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -16,15 +16,42 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle){
+      const id = Math.floor(Math.random() * 100)
+      const newTask: Task = {
+        id,
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks([...tasks,newTask])      
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    // const uniqueTask  = tasks.findIndex( value => value.id === id )
+    // tasks[uniqueTask].isComplete = !tasks[uniqueTask].isComplete    
+    // setTasks(tasks)
+
+    
+    const allTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete : !task.isComplete
+    } : task )
+
+    setTasks(allTasks)
+     
   }
 
-  function handleRemoveTask(id: number) {
+  function handleRemoveTask(id: number): void {
     // Remova uma task da listagem pelo ID
+    setTasks( tasks.filter(value => value.id !== id ) )
   }
+
+  useEffect(()=>{
+
+  },[tasks])
 
   return (
     <section className="task-list container">
